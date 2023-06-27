64149bcc002345a57b40c7f7a1681d2d1b9c13b5
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-27 14:56:20 +0300

    Mise en place de model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+from . import models
+
 
 # Register your models here.
+@register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
