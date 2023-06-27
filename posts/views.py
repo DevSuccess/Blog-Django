3c72f7c5f9742254083d637088a798143911a90e
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-27 15:01:00 +0300

    Ajout de view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+
+from . import models
+
 
 # Create your views here.
+class ArticleList(generic.ListView):
+    queryset = models.Article.objects.filter(status=1)
+    template_name = 'posts/index.html'
+
+
+class ArticleDetail(generic.DetailView):
+    model = models.Article
+    template_name = 'posts/detail.html'
